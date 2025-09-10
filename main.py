4f68270ee09ae6fac9c782404339d83a6758edc6
Spinningskunk <2536684724@qq.com>
2025-09-11 02:17:48 +0800

    solve some question

--- main.py
+++ main.py
@@ -1,70 +1,25 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-import re
-import json
-import os
 
-@register("MarketPulse", "ikun", "updating...", "1.0.0")
+
+@register("test001", "ikun", "一个简单的插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-        self.config_path = os.path.join(os.path.dirname(__file__), "data", "config.json")
-        self.config = {}
-        self.load_config()
 
     async def initialize(self):
-        """异步初始化（如果需要）"""
+        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    def load_config(self):
-        if os.path.exists(self.config_path):
-            try:
-                with open(self.config_path, "r", encoding="utf-8") as f:
-                    self.config = json.load(f)
-            except Exception as e:
-                self.logger.error(f"读取 config.json 失败: {e}")
-                # 兜底默认配置
-                self.config = {
-                    "admin_id": "2536684724",
-                    "reminder_time": "09:00",
-                    "message": "早上好，这是你的每日提醒！"
-                }
-        else:
-            self.logger.warning("配置文件不存在，使用默认配置并创建文件: " + self.config_path)
-            self.config = {
-                "admin_id": "2536684724",
-                "reminder_time": "09:00",
-                "message": "早上好，这是你的每日提醒！"
-            }
-            self.save_config()
-
-    def save_config(self):
-        os.makedirs(os.path.dirname(self.config_path), exist_ok=True)
-        with open(self.config_path, "w", encoding="utf-8") as f:
-            json.dump(self.config, f, ensure_ascii=False, indent=2)
-
-    @filter.command("MarketPulse")
-    async def marketpulse(self, event: AstrMessageEvent):
+    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("test001")
+    async def helloworld(self, event: AstrMessageEvent):
+        """这是一个 hello world 指令"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
-        message_str = event.message_str
-        logger.info(message_str)
-        # match = re.search(r"修改提示时间为\s*(\d{1,2}:\d{2})", message_str)
-        # if match:
-        #     new_time = match.group(1)
-        #     if re.match(r"^(?:[01]?\d|2[0-3]):[0-5]\d$", new_time):
-        #         self.config['reminder_time'] = new_time
-        #         self.save_config()
-        #         yield event.plain_result(f"✅ 已成功将提醒时间修改为 {new_time}")
-        #     else:
-        #         yield event.plain_result("⚠️ 时间格式不正确，请使用 24 小时制，如 09:30 或 23:15")
-        #     return
-        #
-        # if "查看提醒时间" in message_str:
-        #     current_time = self.config.get("reminder_time", "09:00")
-        #     yield event.plain_result(f"⏰ 当前提醒时间为 {current_time}")
-        #     return
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")
-
+        message_str = event.message_str  # 用户发的纯文本消息字符串
+        message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        logger.info(message_chain)
+        yield event.plain_result(f"唉, {user_name}, 你又发了 {message_str}!")  # 发送一条纯文本消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
